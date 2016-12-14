4f36becb1fff59d9e9f09ba3d6ef3e58f0a00fc1
wrmsr <timwilloney@gmail.com>
2016-12-14 02:05:38 -0800

    --

--- src/test/java/com/wrmsr/kleist/file/FileReaderTest.java
+++ src/test/java/com/wrmsr/kleist/file/FileReaderTest.java
@@ -1,16 +1,22 @@
 package com.wrmsr.kleist.file;
 
+import com.wrmsr.kleist.Reader;
 import junit.framework.TestCase;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileReaderTest
         extends TestCase
 {
     public void testIndexReader()
-            throws IOException
+            throws Exception
     {
-        new FileReader(Paths.get("/Users/will/barf"));
+        Path rootPath = Paths.get(getClass().getResource("/test_index/index.json").toURI()).getParent();
+        Reader reader = new FileReader(rootPath);
+        System.out.println(reader.readIndex());
     }
 }
